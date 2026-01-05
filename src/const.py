98518b8ae6f81f284353f4b814f859451ec293f4
zzz <118983639+vzhakir@users.noreply.github.com>
2026-01-06 05:00:41 +0700

    add gravitational constants for jupiter, venus, and mars
    reason? just added for might be additional features in future updates

--- src/const.py
+++ src/const.py
@@ -12,6 +12,10 @@ K_GAUSS = 0.01720209895
 
 # gravitational constant in au^3/day
 MU_SUN = K_GAUSS**2
+MU_JUPITER = MU_SUN * 9.5479193842e-4
+MU_VENUS = MU_SUN * 2.4478383e-6
+MU_MARS = MU_SUN * 3.2271514e-7
 
 # earth radius
 R_EARTH = 4.26352e-5 # au
+R_SUN = 0.00465047 # au
