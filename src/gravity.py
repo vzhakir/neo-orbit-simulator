dde8ced6dde0cb678158343c2e9dcf897b30b626
zzz <118983639+vzhakir@users.noreply.github.com>
2026-01-06 04:59:06 +0700

    add definition for accel total and perturbation calc
    added total acceleration calculation considering both sun and earth gravitational effects.

--- src/gravity.py
+++ src/gravity.py
@@ -3,7 +3,8 @@ gravitational accel models
 """
 
 import numpy as np
-from .const import MU_SUN
+from .const import MU_SUN, MU_EARTH
+from .earth import earth_position
 
 def sun_gravity(r):
     """
@@ -19,3 +20,37 @@ def sun_gravity(r):
     
     norm_r = np.linalg.norm(r)
     return -MU_SUN * r / norm_r**3
+
+def get_total_accel(r, t):
+    """
+    total count for all accel also containing distractions from main planets
+    
+    param:
+    r = ndarray
+        object pos vector (au)
+    t = float
+        time (day)
+    """
+    
+    # main accel from sun and earth distraction
+    accel = sun_gravity(r)
+    r_earth = earth_position(t)
+    accel += _calculate_perturbation(r, r_earth, MU_EARTH)
+    
+    # this was only focusing on sun and earth first, can add more later perturbations from other planets
+    
+    return accel
+
+def _calculate_perturbation(r_obj, r_planet, mu_planet):
+    """
+    third-body gravitational perturbation formula
+    """
+    r_rel = r_obj - r_planet
+    dist_rel = np.linalg.norm(r_rel)
+    dist_planet = np.linalg.norm(r_planet)
+    
+    # direct and indirect component (coor. center)
+    term1 = -mu_planet * r_rel / dist_rel**3 # direct
+    term2 = mu_planet * r_planet / dist_planet**3 # indirect
+    
+    return term1 + term2
